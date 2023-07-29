e3d9699825218bdb555cc59ddd65391b91c4cd10
Dominik <dominik.zemlik.dev@gmail.com>
2023-07-29 15:11:41 +0200

    Separate view from logic

--- index.php
+++ index.php
@@ -2,34 +2,12 @@
   declare(strict_types=1);
   namespace App;
   require_once("src/utilities/debug.php");
-?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Notes App</title>
-</head>
-<body>
-  <header>
-    <h1>My notes</h1>
-  </header>
-
-  <div>
-    <nav>
-      <ul>
-        <li>
-          <a href="/">List notes</a>
-        </li>
-         <li>
-          <a href="/?action=create">New note</a>
-        </li>
-      </ul>
-    </nav>
-    <main>Content</main>
-  </div>
   
-  <footer>Footer</footer>
-</body>
-</html>
+  $action = $_GET['action'] ?? null;
+
+
+  if($action === 'create') {
+    include_once('templates/pages/create.php');
+  } else {
+    include_once('templates/pages/list.php');
+  }
